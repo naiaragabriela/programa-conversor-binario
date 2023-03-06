729659248a501eafcea7abbc3ecedd4feb5dffa5
naiaragabriela <naigabi31@gmail.com>
2023-03-06 19:32:49 -0300

    conversão binário usando função

---
+++ src/ProgramaConversorBinario/ProgramaConversorBinario/Program.cs
@@ -0,0 +1,30 @@
+﻿int numero, resto;
+
+int[] binario = new int[8];
+
+int numeroDigitado()
+{
+    Console.WriteLine("Informe um número: ");
+    return int.Parse(Console.ReadLine()); 
+}
+numero = numeroDigitado();
+
+int converterBinario()
+{
+    resto = numero % 2;
+    numero = numero / 2;
+    return resto;
+}
+
+int contador = 7;
+do
+{
+    binario[contador] = converterBinario();
+    contador--;
+} while (contador >= 0);
+
+
+for (int posicao = 0; posicao < 8; posicao++)
+{
+    Console.Write(binario[posicao] + " ");
+}
