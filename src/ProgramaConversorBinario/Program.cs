2c2dd68314378f1c91da29d786576d993cd485be
naiaragabriela <naigabi31@gmail.com>
2023-03-06 21:12:31 -0300

    conversao de binario utilizando funcao recursiva

--- src/ProgramaConversorBinario/Program.cs
+++ src/ProgramaConversorBinario/Program.cs
@@ -1,25 +1,26 @@
-﻿int valor, resto, quociente;
+﻿int numero, resto, quociente;
+int[] posicao = new int[8];
 
-int[] binario = new int[8];
-int[] convertido = new int[8];
+Console.WriteLine("Informe um número: ");
+numero = int.Parse(Console.ReadLine());
 
-Console.WriteLine("Informe um número inteiro a ser convertido");
-valor = int.Parse(Console.ReadLine());
-
-convertido = converter(valor);
-int[] converter (int valor)
+int binario (int numero) 
 {
-    quociente = valor;
-    for (int i = 0; i < binario.Length; i++)
+    quociente = numero;
+
+    if ( numero == 0)
+    {
+        return numero;
+    }
+    else
     {
         resto = quociente % 2;
-        binario[i] = resto;
-        quociente = valor / 2;
-        valor = quociente;
+        posicao[numero] = resto;
+        quociente = numero;
+        numero = quociente;
+
+        return binario(numero - 1);
     }
-    return binario;
+
 }
- for (int i = binario.Length -1; i>= 0; i--)
-{
-    Console.Write(binario[i] + " ");
-}
+Console.Write(binario(numero));
